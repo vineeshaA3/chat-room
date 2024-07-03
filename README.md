349693ba6f71379c06f1691d49716a78f13a14fc
vineeshaA3 <98724891+vineeshaA3@users.noreply.github.com>
2024-07-03 17:07:38 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
-# chat-room
+# Chat App
+
+Real time chat app built with React, Express and Socket.io.
+
+![Chat](https://i.imgur.com/BxI8MOz.png)
+
+## Features
+
+1. Log-in screen
+2. Online users list
+3. Input field
+4. Message board
+5. Chat bot messages
+6. Connecting via room name
+
+## Installation
+
+1. `git clone https://github.com/madzadev/chat.git`
+
+2. `cd chat`
+
+3. `cd server && npm install` and `npm run server`
+
+4. `cd client && npm install` and `npm start`
+
+## Contributions
+
+Any feature requests and pull requests are welcome!!
+
+## License
+
+The project is under [MIT license](https://choosealicense.com/licenses/mit/).
---
+++ client/src/components/Message/Message.js
@@ -0,0 +1,43 @@
+import React from "react";
+import ReactEmoji from "react-emoji";
+import ProfileIcon from "./../../icons/profile.png";
+import RobotIcon from "./../../icons/robot.png";
+
+import "./Message.css";
+
+const Message = ({ message: { user, text }, name }) => {
+  let isSentByCurrentUser = false;
+  let isSentByAdmin = false;
+
+  if (user === "admin") {
+    isSentByAdmin = true;
+  } else if (user.toLowerCase() === name.toLowerCase()) {
+    isSentByCurrentUser = true;
+  }
+
+  return isSentByCurrentUser ? (
+    <div className="messageContainer justifyEnd">
+      <div className="messageBox backgroundOrange">
+        <p className="messageText colorWhite">{ReactEmoji.emojify(text)}</p>
+      </div>
+      <div className="profile-box user">
+        <img className="profile-icon" src={ProfileIcon} alt="img" />
+      </div>
+    </div>
+  ) : (
+    <div className="messageContainer justifyStart">
+      <div className="profile-box robot">
+        <img
+          className="profile-icon"
+          src={isSentByAdmin ? RobotIcon : ProfileIcon}
+          alt="img"
+        />
+      </div>
+      <div className="messageBox backgroundLight">
+        <p className="messageText colorDark">{ReactEmoji.emojify(text)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Message;
